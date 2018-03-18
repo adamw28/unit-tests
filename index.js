3f61bc9cc46355bf77718cd6321b39058005486c
Adam Więcek <adamw28@gmail.com>
2018-03-19 00:05:37 +0100

    Run four unit tests

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const formatDate = (timeInSeconds) => {
-  let seconds = minutes = hours = 0;
+  let seconds = timeInSeconds;
+  let minutes = hours = 0;
   if (!timeInSeconds || timeInSeconds == '')
   	return `0s`;
   else {
@@ -9,10 +10,10 @@ const formatDate = (timeInSeconds) => {
   		do{
   			seconds = seconds - 60;
   			minutes++;
-  		}while(seconds>=60);
+  		}while(seconds >= 60);
   	}
 
-  	return `${minutes}s ${seconds}s`;
+  	return `${minutes}m ${seconds}s`;
   }
 }
 
