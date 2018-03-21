d70ba5c09e7ef1d33abce933c76884ecbaa861e8
Adam Więcek <adamw28@gmail.com>
2018-03-21 02:19:43 +0100

    Run ten unit tests

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 const formatDate = (timeInSeconds) => {
   let seconds = timeInSeconds;
   let minutes = hours = 0;
+  let message = '';
+
   if (!timeInSeconds || timeInSeconds == '')
   	return `0s`;
   else {
-  	if(timeInSeconds < 60)
-  		return `${timeInSeconds}s`;
-  	else{
-  		do{
-  			seconds = seconds - 60;
-  			minutes++;
-  		}while(seconds >= 60);
-  	}
-
-  	return `${minutes}m ${seconds}s`;
+    while(seconds >= 3600){
+      seconds = seconds - 3600;
+      hours++;
+    }
+    if(hours > 0) message = message + `${hours}h`;
+  	while(seconds >= 60){
+      seconds = seconds - 60;
+      minutes++;
+    }
+    if(minutes > 0) {
+    if(message)message = message + ` `;
+      message = message + `${minutes}m`;
+    }
+    if(seconds > 0){
+    if(message)message = message + ` `;
+      message = message + `${seconds}s`;
+    }
   }
+    return message;
 }
 
 module.exports = formatDate;
--- index.test.js
+++ index.test.js
@@ -44,12 +44,40 @@ describe('formatDate', () => {
   });
 
   describe('hours', () => {
-    it('should return hours', () => {
-  		const actual = formatDate(23);
+    it('should work for one digit hours, minutes and seconds', () => {
+      const actual = formatDate(3743);
 
-  		const expected = '0 hours 0 minutes 23s';
+      const expected = '1h 2m 23s';
+
+      assert.equal(actual, expected);
+  });
+    it('should work for two digits hours, minutes and seconds', () => {
+      const actual = formatDate(7355);
+
+      const expected = '2h 2m 35s';
+
+      assert.equal(actual, expected);
+  });
+    it('should work for hours only', () => {
+  		const actual = formatDate(7200);
+
+  		const expected = '2h';
 
   		assert.equal(actual, expected);
 	});
+    it('should work for hours and minutes', () => {
+      const actual = formatDate(7320);
+
+      const expected = '2h 2m';
+
+      assert.equal(actual, expected);
+  });
+    it('should work for hours and seconds', () => {
+      const actual = formatDate(7255);
+
+      const expected = '2h 55s';
+
+      assert.equal(actual, expected);
+  });
   });
 });
